d1bc6d78fddf1514da6fe8e48e5697d8a43e2924
mateuszherczka <mateuszherczka@kth.fake>
2015-04-26 09:42:01 +0200

    cleaned up neighbour definitions.

--- mpitests/psplit.c
+++ mpitests/psplit.c
@@ -113,7 +113,8 @@ int main(int argc, char **argv)
 	// else printf("MeshRank %d is black.\n", meshRank);
 
 	// get neighbours rank within mesh communicator
-	int hood[3][3];	// hood[i][j] = meshRank, hood[1][1] = meshRank
+	int kernel[3][3];		// hood[i][j] = meshRank, hood[1][1] = my meshRank
+	int neighbour[ndirs];	// neighbour[DIRECTION] = rank of neighbour
 	//int nRank, eRank, sRank, wRank, neRank, seRank, swRank, nwRank;
 	for (int i = 0; i < 3; ++i)
 	{
@@ -124,19 +125,17 @@ int main(int argc, char **argv)
 			int crd[2] = {xof + i, yof + j};
 			int rnk;
 			MPI_Cart_rank(processMesh, crd, &rnk); 	// get rank
-			hood[i][j] = rnk;
+			kernel[i][j] = rnk;
 		}
 	}
-
-	// if (meshRank == 0)
-	// {
-	// 	printf("Rank: %d, meshRank: %d, i, j: %d,%d, hood: %d %d %d %d %d %d %d %d %d\n",
-	// 	       rank, meshRank, meshCoords[0], meshCoords[1],
-	// 	       hood[0][0], hood[0][1], hood[0][2],
-	// 	       hood[1][0], hood[1][1], hood[1][2],
-	// 	       hood[2][0], hood[2][1], hood[2][2]
-	// 	      );
-	// }
+	neighbour[NORTH] = kernel[0][1];
+	neighbour[EAST] = kernel[1][2];
+	neighbour[SOUTH] = kernel[2][1];
+	neighbour[WEST] = kernel[0][1];
+	neighbour[NORTHEAST] = kernel[0][2];
+	neighbour[SOUTHEAST] = kernel[2][2];
+	neighbour[SOUTHWEST] = kernel[0][2];
+	neighbour[NORTHWEST] = kernel[0][0];
 
 	//=========================================================================
 	// Submatrices for border(outgoing) and ghost(incoming)
@@ -159,17 +158,6 @@ int main(int argc, char **argv)
 	                        1, 1,			//  SW size
 	                        1, 1			//  NW size
 	                   };
-
-	/*int ghostStarts[] = {	0, 1,
-	                        1, n + 1,		//	east starts
-	                        n + 1, 1,		// 	south starts
-	                        1, 0,			//	west starts
-	                        0, n + 1,		// 	NE starts
-	                        n + 1, n + 1, 	// 	SE starts
-	                        n + 1, 0,		// 	SW starts
-	                        0, 0			// 	NW starts
-	                    };*/
-
 	// create the border subarrays
 	for (int i = 0; i < ndirs; ++i)
 	{
@@ -194,16 +182,6 @@ int main(int argc, char **argv)
 	                        1, 1			//  NW size
 	                    };
 
-	/*int borderStarts[] = {	1, 1,			// 	north starts
-	                        1, n,			//	east starts
-	                        n, 1,			// 	south starts
-	                        1, 1,			//	west starts
-	                        1, n,			// 	NE starts
-	                        n, n,		 	// 	SE starts
-	                        n, 1,			// 	SW starts
-	                        1, 1,			// 	NW starts
-	                     };*/
-
 	// create the border subarrays
 	for (int i = 0; i < ndirs; ++i)
 	{
@@ -213,10 +191,6 @@ int main(int argc, char **argv)
 		MPI_Type_create_subarray(2, sizes, subsizes, starts, MPI_ORDER_C, MPI_DOUBLE, &border[i]);
 		MPI_Type_commit(&border[i]);
 	}
-
-
-
-
 
 	//=========================================================================
 	// Computation
@@ -233,7 +207,6 @@ int main(int argc, char **argv)
 			localData[i][j] = (i * (n + 2)) + j + 1 + rank * 100;
 		}
 	}
-
 
 	// send stuff to neighbours
 	int err = 0;
@@ -252,15 +225,15 @@ int main(int argc, char **argv)
 	{
 		printf("rank %d sending to %d.\n", rank, 2 );
 		// err = MPI_Send(&(localData[0][1]), 1, border[NORTH], 2, 666, MPI_COMM_WORLD);
-		err = MPI_Send(&(DATA_BORDER_NORTH), 1, border[NORTH], NBR_NORTH, 666, MPI_COMM_WORLD);
-		err = MPI_Send(&(DATA_BORDER_EAST), n, border[EAST], NBR_NORTH, 666, MPI_COMM_WORLD);
-		err = MPI_Send(&(DATA_BORDER_SOUTH), 1, border[SOUTH], NBR_NORTH, 666, MPI_COMM_WORLD);
-		err = MPI_Send(&(DATA_BORDER_WEST), n, border[WEST], NBR_NORTH, 666, MPI_COMM_WORLD);
+		err = MPI_Send(&(DATA_BORDER_NORTH), 1, border[NORTH], neighbour[NORTH], 666, MPI_COMM_WORLD);
+		err = MPI_Send(&(DATA_BORDER_EAST), n, border[EAST], neighbour[NORTH], 666, MPI_COMM_WORLD);
+		err = MPI_Send(&(DATA_BORDER_SOUTH), 1, border[SOUTH], neighbour[NORTH], 666, MPI_COMM_WORLD);
+		err = MPI_Send(&(DATA_BORDER_WEST), n, border[WEST], neighbour[NORTH], 666, MPI_COMM_WORLD);
 
-		err = MPI_Send(&(DATA_BORDER_NORTHEAST), 1, MPI_DOUBLE, NBR_NORTH, 666, MPI_COMM_WORLD);
-		err = MPI_Send(&(DATA_BORDER_SOUTHEAST), 1, MPI_DOUBLE, NBR_NORTH, 666, MPI_COMM_WORLD);
-		err = MPI_Send(&(DATA_BORDER_SOUTHWEST), 1, MPI_DOUBLE, NBR_NORTH, 666, MPI_COMM_WORLD);
-		err = MPI_Send(&(DATA_BORDER_NORTHWEST), 1, MPI_DOUBLE, NBR_NORTH, 666, MPI_COMM_WORLD);
+		err = MPI_Send(&(DATA_BORDER_NORTHEAST), 1, MPI_DOUBLE, neighbour[NORTH], 666, MPI_COMM_WORLD);
+		err = MPI_Send(&(DATA_BORDER_SOUTHEAST), 1, MPI_DOUBLE, neighbour[NORTH], 666, MPI_COMM_WORLD);
+		err = MPI_Send(&(DATA_BORDER_SOUTHWEST), 1, MPI_DOUBLE, neighbour[NORTH], 666, MPI_COMM_WORLD);
+		err = MPI_Send(&(DATA_BORDER_NORTHWEST), 1, MPI_DOUBLE, neighbour[NORTH], 666, MPI_COMM_WORLD);
 
 		// printf("Send step done, err: %d.\n", err);
 
@@ -271,15 +244,15 @@ int main(int argc, char **argv)
 
 		printf("MeshRank %d receiving from %d.\n", rank, 0 );
 
-		err = MPI_Recv(&(DATA_GHOST_SOUTH), 1, ghost[SOUTH], NBR_SOUTH, 666, MPI_COMM_WORLD, &status);
-		err = MPI_Recv(&(DATA_GHOST_WEST), n, ghost[WEST], NBR_SOUTH, 666, MPI_COMM_WORLD, &status);
-		err = MPI_Recv(&(DATA_GHOST_NORTH), 1, ghost[NORTH], NBR_SOUTH, 666, MPI_COMM_WORLD, &status);
-		err = MPI_Recv(&(DATA_GHOST_EAST), n, ghost[WEST], NBR_SOUTH, 666, MPI_COMM_WORLD, &status);
+		err = MPI_Recv(&(DATA_GHOST_SOUTH), 1, ghost[SOUTH], neighbour[SOUTH], 666, MPI_COMM_WORLD, &status);
+		err = MPI_Recv(&(DATA_GHOST_WEST), n, ghost[WEST], neighbour[SOUTH], 666, MPI_COMM_WORLD, &status);
+		err = MPI_Recv(&(DATA_GHOST_NORTH), 1, ghost[NORTH], neighbour[SOUTH], 666, MPI_COMM_WORLD, &status);
+		err = MPI_Recv(&(DATA_GHOST_EAST), n, ghost[WEST], neighbour[SOUTH], 666, MPI_COMM_WORLD, &status);
 		
-		err = MPI_Recv(&(DATA_GHOST_SOUTHWEST), n, MPI_DOUBLE, NBR_SOUTH, 666, MPI_COMM_WORLD, &status);
-		err = MPI_Recv(&(DATA_GHOST_NORTHWEST), n, MPI_DOUBLE, NBR_SOUTH, 666, MPI_COMM_WORLD, &status);
-		err = MPI_Recv(&(DATA_GHOST_NORTHEAST), n, MPI_DOUBLE, NBR_SOUTH, 666, MPI_COMM_WORLD, &status);
-		err = MPI_Recv(&(DATA_GHOST_SOUTHEAST), n, MPI_DOUBLE, NBR_SOUTH, 666, MPI_COMM_WORLD, &status);
+		err = MPI_Recv(&(DATA_GHOST_SOUTHWEST), n, MPI_DOUBLE, neighbour[SOUTH], 666, MPI_COMM_WORLD, &status);
+		err = MPI_Recv(&(DATA_GHOST_NORTHWEST), n, MPI_DOUBLE, neighbour[SOUTH], 666, MPI_COMM_WORLD, &status);
+		err = MPI_Recv(&(DATA_GHOST_NORTHEAST), n, MPI_DOUBLE, neighbour[SOUTH], 666, MPI_COMM_WORLD, &status);
+		err = MPI_Recv(&(DATA_GHOST_SOUTHEAST), n, MPI_DOUBLE, neighbour[SOUTH], 666, MPI_COMM_WORLD, &status);
 
 		// err = MPI_Recv(&(localData[0][1]), 1, ghost[SOUTH], 0, 666, MPI_COMM_WORLD, &status);
 
@@ -289,11 +262,6 @@ int main(int argc, char **argv)
 		MPI_Get_count(&status, ghost[SOUTH], &rcount);
 
 		printf("Received: %d.\n", rcount);
-		if (rank == 2) {
-
-			printf("rank %d reporting.\n", rank );
-			printarr(localData, n + 2, "LocalData after comm.");
-		}
 
 	}
 
@@ -301,9 +269,7 @@ int main(int argc, char **argv)
 	// Output
 	//=========================================================================
 
-
-
-	/*if (meshRank == 0) {
+	if (meshRank == 0) {
 
 		printf("MeshRank %d reporting.\n", meshRank );
 		printarr(localData, n + 2, "LocalData after comm.");
@@ -330,7 +296,7 @@ int main(int argc, char **argv)
 		            rank + 1,
 		            666,
 		            MPI_COMM_WORLD);
-	}*/
+	}
 
 	//=========================================================================
 	// Cleanup
